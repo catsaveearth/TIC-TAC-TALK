be4f9eeda1fa78fe92a5f081788e49e03311770c
shin269 <75072742+shin269@users.noreply.github.com>
2020-12-13 19:22:14 +0900

    Add files via upload
    Summary - PPT index & function video

---
+++ PPT_Index_video.java
@@ -0,0 +1,59 @@
+
+/* // PPT_index //
+1. Team 1, student name/number,
+   Subject: -Chatting programe 
+
+2. Table(index) of contents
+
+3. Main, Chat (or other screen) captured screen upload
+
+4. To list basic functions and additional functions.
+
+5. Method of using (each funtions)
+
+6. structure diagram
+
+7. github link (code, Demonstration video, others)
+
+8. Major rule sharing
+
+9. daily meetinh
+
+10. Tank you.
+*/
+
+
+
+/* // Requirement //
+ 1. Access and sign up/withdraw
+
+ 2. Login function
+
+ 3. User/friend manage
+
+ 4. Chatting room function
+
+ 5. File transmission
+
+ 6. Public data use
+
+ 7. Additional function
+*/
+
+
+
+/* // Demonstation video //
+1. Sign-up - Login
+
+2. Main screen (public data)
+
+3. Friend reqyest - friend accept
+
+4. 1:1 Chatting 
+
+5. Multiple Chatting
+
+6. File transmission - File reception
+
+7. Mini_game 'TicTacToe(1:1)' execution
+*/
